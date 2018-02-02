84f00c7dde58115cd0cf4ba358f989002c3f99cd
Carrie Mims <carrie.mims@gmail.com>
2018-02-02 17:20:02 -0500

    updated styles

--- js/main.js
+++ js/main.js
@@ -15,9 +15,120 @@ function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
         center: myLocation,
         zoom: 12,
-      });
-      var bikeLayer = new google.maps.BicyclingLayer();
-        bikeLayer.setMap(map);
+        mapTypeId: 'roadmap',
+        styles: [
+          { "elementType": "geometry",
+            "stylers": [{ "color": "#ebe3cd"}]},
+          
+          { "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#523735" }]},
+          
+          { "elementType": "labels.text.stroke",
+            "stylers": [{ "color": "#f5f1e6" }]},
+          
+          { "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [{ "color": "#c9b2a6" }]},
+          
+          { "featureType": "administrative.land_parcel",
+            "stylers": [{ "visibility": "off" }]},
+          
+          { "featureType": "administrative.land_parcel",
+            "elementType": "geometry.stroke",
+            "stylers": [{ "color": "#dcd2be"}]},
+          
+          { "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#ae9e90" }]},
+  
+          { "featureType": "administrative.neighborhood",
+            "stylers": [{ "visibility": "off" }]},
+  
+          { "featureType": "landscape.natural",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#dfd2ae" }]},
+  
+          { "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#dfd2ae" }]},
+  
+          { "featureType": "poi",
+            "elementType": "labels.text",
+            "stylers": [{ "visibility": "off" }]},
+  
+          { "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#93817c" }]},
+  
+          { "featureType": "poi.park",
+            "elementType": "geometry.fill",
+            "stylers": [{ "color": "#a5b076" }]},
+  
+          { "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#447530" }]},
+  
+          { "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#f5f1e6" }]},
+  
+          { "featureType": "road",
+            "elementType": "labels",
+            "stylers": [{ "visibility": "off" }]},
+ 
+          { "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#fdfcf8" }]},
+  
+          { "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#f8c967" }]},
+  
+          { "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{ "color": "#e9bc62" }]},
+  
+          { "featureType": "road.highway.controlled_access",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#e98d58" }]},
+  
+          { "featureType": "road.highway.controlled_access",
+            "elementType": "geometry.stroke",
+            "stylers": [{ "color": "#db8555" }]},
+  
+          { "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#806b63" }]},
+  
+          { "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#dfd2ae" }]},
+  
+          { "featureType": "transit.line",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#8f7d77" }]},
+  
+          { "featureType": "transit.line",
+            "elementType": "labels.text.stroke",
+            "stylers": [{ "color": "#ebe3cd" }]},
+  
+          { "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#dfd2ae" }]},
+  
+          { "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [{ "color": "#3696b1" }]},
+  
+          { "featureType": "water",
+            "elementType": "labels.text",
+            "stylers": [{ "visibility": "off" }]},
+  
+          { "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#92998d" }]
+  }]
+});
       
       infoWindow = new google.maps.InfoWindow({
         map: map,
@@ -62,31 +173,36 @@ function createMarker(place) {
   };
   var service = new google.maps.places.PlacesService(map);
     service.getDetails(request, function(details, status) {
-  var photoUrl = details.photos[0].getUrl({maxWidth: 200});
+  var photoUrl = details.photos[0].getUrl({maxWidth: 640});
+      infoWindow.setContent(details.name);
       $("#pub_info").empty().append([
-      '<div class="place-img"><img src="' + photoUrl + '"</div>',
-      '<div><a href="' + details.url + '">' + details.name + '</a></br>',
-      '<button type="submit" id="button">Add to Pub List</button></br>',
-      details.vicinity,
-      details.opening_hours.open_now = "Open Now",
-      details.rating,
-      details.formatted_phone_number + '</div>'].join("<br />"));
+      '<div class="pub-photo"><img src="' + photoUrl + '"class="center-cropped"></div>' +
+      '<div class="pub-name"><a href="' + details.url + '" class="pub-link">' + details.name + '</a><span class="right white">' + details.rating + '</span></div>' + 
+      '<div class="pub-details">' + details.vicinity,
+//      details.opening_hours.open_now = "Open Now",
+//      details.rating,
+      details.formatted_phone_number + 
+      '<div><button type="submit" id="button" class="main-btn">Add to Pub List</button></br>' +
+      '</div>'].join("<br />"));
       $('#button').click(function(e) {
                e.preventDefault();
-                $('#pub_list ul').append('<li><span class="pub-name">' + details.name + '</span><div class="pub-controls"><button class="pub-toggle"><span class="visited">visited</span></button> <button class="pub-delete"><span class="delete">remove</span></button></div></li>');
+                $('#pub_list ul').append('<li><span class="pub-name-list">' + details.name + '</span><div class="pub-controls"><img src="http://image.ibb.co/b45EB6/Red_X_in_circle_small.png" class="pub-delete" alt="Click X to Remove Pub from List" border="0"></div></li>');
             });
 
             infoWindow.open(map, marker);
         });
 
-
         $('.my-pub-list').on('click', '.pub-delete', function(event) {
             $(this).closest('li').remove();
         });
-        $('.my-pub-list').on('click', '.pub-toggle', function(event) {
-            $(this).closest('li').find('.pub-name').toggleClass('my-pub-list__visited');
-        });
+//        $('.my-pub-list').on('click', '.pub-toggle', function(event) {
+//            $(this).closest('li').find('.pub-name-list').toggleClass('my-pub-list__visited');
+//        });
     });
+}
+
+function savedPubs() {
+  
 }
 
 
